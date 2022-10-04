a6a6a5e1695543c754c297ffa9c519ac3ac3cf7e
eguerrerojr <74942638+eguerrerojr@users.noreply.github.com>
2022-10-04 09:05:47 +0800

    Add files via upload

--- javascript/contactus.js
+++ javascript/contactus.js
@@ -1,44 +1,74 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAfk9oorgez6T8VvBZkE3Fub3ePxrascW0",
-    authDomain: "contact-emb.firebaseapp.com",
-    databaseURL: "https://contact-emb-default-rtdb.firebaseio.com",
-    projectId: "contact-emb",
-    storageBucket: "contact-emb.appspot.com",
-    messagingSenderId: "774470923232",
-    appId: "1:774470923232:web:d7e210d87993b4bc76b185"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAfk9oorgez6T8VvBZkE3Fub3ePxrascW0",
+//     authDomain: "contact-emb.firebaseapp.com",
+//     databaseURL: "https://contact-emb-default-rtdb.firebaseio.com",
+//     projectId: "contact-emb",
+//     storageBucket: "contact-emb.appspot.com",
+//     messagingSenderId: "774470923232",
+//     appId: "1:774470923232:web:d7e210d87993b4bc76b185"
+//   };
 
-  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
 
-  var contactusDB = firebase.database().ref("contactform");
-  document.getElementById("contactus").addEventListener("submit", submitForms)
+//   var contactusDB = firebase.database().ref("contactform");
+//   document.getElementById("contactus").addEventListener("submit", submitForms)
 
-  function submitForms(e){
-    e.preventDefault();
+//   function submitForms(e){
+//     e.preventDefault();
 
-    var txtName = getElementVal("txtName");
-    var txtEmail = getElementVal("txtEmail");
-    var txtSubject = getElementVal("txtSubject");
-    var txtMessage = getElementVal("txtMessage");
+//     var txtName = getElementVal("txtName");
+//     var txtEmail = getElementVal("txtEmail");
+//     var txtSubject = getElementVal("txtSubject");
+//     var txtMessage = getElementVal("txtMessage");
    
-     saveMessages(txtName, txtEmail, txtSubject, txtMessage);
+//      saveMessages(txtName, txtEmail, txtSubject, txtMessage);
 
-     alert("Sent successfully :) we will contact you shortly")
-     document.getElementById("contactus").reset();
-  }
-  const saveMessages = (txtName, txtEmail, txtSubject, txtMessage) => {
-    var newContactForms = contactusDB.push();
+//      alert("Sent successfully :) we will contact you shortly")
+//      document.getElementById("contactus").reset();
+//   }
+//   const saveMessages = (txtName, txtEmail, txtSubject, txtMessage) => {
+//     var newContactForms = contactusDB.push();
 
-    newContactForms.set({
-        txtName: txtName,
-        txtEmail: txtEmail,
-        txtSubject: txtSubject,
-        txtMessage: txtMessage,
+//     newContactForms.set({
+//         txtName: txtName,
+//         txtEmail: txtEmail,
+//         txtSubject: txtSubject,
+//         txtMessage: txtMessage,
         
-    });
-  };
+//     });
+//   };
 
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  };
-  
+//   const getElementVal = (id) => {
+//     return document.getElementById(id).value;
+//   };
+
+// function sendEmail() {
+// 	Email.send({
+//     SecureToken : "8168992f-ac19-4b33-befb-21b1802e25ea",
+//     To : 'gwapoko0407@gmail.com',
+//     From : "mozbygreen@gmail.com",
+//     Subject : "Test Email",
+//     Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+//     }).then(
+//        alert("mail sent successfully")
+//     );
+// }
+
+function sendEmail() {
+    let name = document.getElementById("txtName").value;
+    let email = document.getElementById("txtEmail").value;
+    let subject = document.getElementById("txtSubject").value;
+    let message = document.getElementById("txtMessage").value;
+    let finalmessage = `Name : ${name} <br>  Email : ${email} <br> Subject: ${subject} <br>  Message : ${message} <br>`;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "mozbygreen13@gmail.com",
+        Password : "07431A51AA5A23C51F6ECEB97C0E1BBDD826",
+        To : 'mozbygreen13@gmail.com',
+        From : "mozbygreen13@gmail.com",
+        Subject : "EMB",
+        Body : finalmessage
+    }).then(
+       alert("Sent Successfully")
+    );
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,51 @@
-$(document).ready(function(){
-    $(".text-room1").hover(function(){
-        $(".text-white1").css("color", "white")
-        ,$(".triangle-left").css("border-right", "27px solid #E0BB20")
-        ,$(".view-button").css("background-color", "#ffff").css("border", "none");
+// $(document).ready(function(){
+//     $(".text-room1").hover(function(){
+//         $(".text-white1").css("color", "white")
+//         ,$(".triangle-left").css("border-right", "27px solid #E0BB20")
+//         ,$(".view-button").css("background-color", "#ffff").css("border", "none");
         
-    },
-    function(){
-        $(".text-white1").css("color", "var(--newdarkgreen)")
-        ,$(".triangle-left").css("border-right", "27px solid white")
-        ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
-    }); 
-  });
+//     },
+//     function(){
+//         $(".text-white1").css("color", "var(--newdarkgreen)")
+//         ,$(".triangle-left").css("border-right", "27px solid white")
+//         ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
+//     }); 
+//   });
 
-// second
+// // second
 
-  $(".text-room2").hover(function(){
-    $(".text-white2").css("color", "white")
-    ,$(".triangle-left1").css("border-right", "27px solid #E0BB20")
-    ,$(".view-button").css("background-color", "#ffff").css("border", "none");
-  },
-  function(){
-    $(".text-white2").css("color", "var(--newdarkgreen)")
-    ,$(".triangle-left1").css("border-right", "27px solid white")
-    ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
-  });
+//   $(".text-room2").hover(function(){
+//     $(".text-white2").css("color", "white")
+//     ,$(".triangle-left1").css("border-right", "27px solid #E0BB20")
+//     ,$(".view-button").css("background-color", "#ffff").css("border", "none");
+//   },
+//   function(){
+//     $(".text-white2").css("color", "var(--newdarkgreen)")
+//     ,$(".triangle-left1").css("border-right", "27px solid white")
+//     ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
+//   });
 
-// third
+// // third
 
-  $(".text-room3").hover(function(){
-    $(".text-white3").css("color", "white")
-    ,$(".view-button").css("background-color", "#ffff").css("border", "none");
-  },
-  function(){
-    $(".text-white3").css("color", "var(--newdarkgreen)")
-    ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
-  }); 
+//   $(".text-room3").hover(function(){
+//     $(".text-white3").css("color", "white")
+//     ,$(".view-button").css("background-color", "#ffff").css("border", "none");
+//   },
+//   function(){
+//     $(".text-white3").css("color", "var(--newdarkgreen)")
+//     ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
+//   }); 
 
-// fourth
+// // fourth
 
-  $(".text-room4").hover(function(){
-    $(".text-white4").css("color", "white")
-    ,$(".view-button").css("background-color", "#ffff").css("border", "none");
-  },
-  function(){
-    $(".text-white4").css("color", "var(--newdarkgreen)")
-    ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
-  }); 
+//   $(".text-room4").hover(function(){
+//     $(".text-white4").css("color", "white")
+//     ,$(".view-button").css("background-color", "#ffff").css("border", "none");
+//   },
+//   function(){
+//     $(".text-white4").css("color", "var(--newdarkgreen)")
+//     ,$(".view-button").css("background-color", "transparent").css("border", "2px solid rgba(0,0,0,.1");
+//   }); 
 
 
 
